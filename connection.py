fe3f754d79e6d77450a9f36080ba6b86b9c8f102
Anderson Pozo <anderson.pozo@upec.edu.ec>
2021-11-10 23:42:52 -0500

    Update requirements

--- connection.py
+++ connection.py
@@ -2,6 +2,6 @@ import pymongo
 
 
 def get_database():
-    connection_string = "mongodb://localhost:27017/backup"
+    connection_string = "mongodb+srv://<USER>:<PASSWORD>X@cluster0.owcnv.mongodb.net/backup?retryWrites=true&w=majority"
     client = pymongo.MongoClient(connection_string)
     return client['backup']
